9c91f613318f7e7c5fd4b74dad18c8f03ddff549
imyzp <601405772yzp>
2022-02-26 21:06:08 +0800

    localdatetime

--- JavaSamples/javasamples1_8/src/main/java/com/yzp/javasamples1_8/time/LocalDateTimeTest.java
+++ JavaSamples/javasamples1_8/src/main/java/com/yzp/javasamples1_8/time/LocalDateTimeTest.java
@@ -1,8 +1,73 @@
 package com.yzp.javasamples1_8.time;
 
+import java.time.*;
+import java.util.Date;
+
 /**
  * https://www.cnblogs.com/huanshilang/p/12013386.html
  */
 public class LocalDateTimeTest {
+    /**
+     * LocalDateTime转毫秒时间戳
+     * @param localDateTime LocalDateTime
+     * @return 时间戳
+     */
+    public static Long localDateTimeToTimestamp(LocalDateTime localDateTime) {
+        try {
+            ZoneId zoneId = ZoneId.systemDefault();
+            Instant instant = localDateTime.atZone(zoneId).toInstant();
+            return instant.toEpochMilli();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    /**
+     * 时间戳转LocalDateTime
+     * @param timestamp 时间戳
+     * @return LocalDateTime
+     */
+    public static LocalDateTime timestampToLocalDateTime(long timestamp) {
+        try {
+            Instant instant = Instant.ofEpochMilli(timestamp);
+            ZoneId zone = ZoneId.systemDefault();
+            return LocalDateTime.ofInstant(instant, zone);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Date转LocalDateTime
+     * @param date Date
+     * @return LocalDateTime
+     */
+    public static LocalDateTime dateToLocalDateTime(Date date) {
+        try {
+            Instant instant = date.toInstant();
+            ZoneId zoneId = ZoneId.systemDefault();
+            return instant.atZone(zoneId).toLocalDateTime();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * LocalDateTime转Date
+     * @param localDateTime LocalDateTime
+     * @return Date
+     */
+    public static Date localDateTimeToDate(LocalDateTime localDateTime) {
+        try {
+            ZoneId zoneId = ZoneId.systemDefault();
+            ZonedDateTime zdt = localDateTime.atZone(zoneId);
+            return Date.from(zdt.toInstant());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
