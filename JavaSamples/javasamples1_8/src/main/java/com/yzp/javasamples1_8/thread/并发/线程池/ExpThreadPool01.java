bdcd38f6637337854090e113c71fb2fa2417f7cb
imyzp <18065220862.163.com>
2022-03-18 21:07:18 +0800

    线程池

---
+++ JavaSamples/javasamples1_8/src/main/java/com/yzp/javasamples1_8/thread/并发/线程池/ExpThreadPool01.java
@@ -0,0 +1,27 @@
+package com.yzp.javasamples1_8.thread.并发.线程池;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExpThreadPool01 {
+    public static void main(String[] args) {
+        // 创建线程池服务
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        // 执行
+        executorService.execute(new MyThread());
+        executorService.execute(new MyThread());
+        executorService.execute(new MyThread());
+        executorService.execute(new MyThread());
+
+        // 关闭连接
+        executorService.shutdown();
+    }
+}
+
+class MyThread implements Runnable{
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
